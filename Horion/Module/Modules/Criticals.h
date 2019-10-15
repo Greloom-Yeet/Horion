ad7e24eb2cc82fcfc2e9aff341a52adb776a16aa
d1gress <55198830+d1gress@users.noreply.github.com>
2019-10-15 08:19:11 +0800

    start on crit chance

--- Horion/Module/Modules/Criticals.h
+++ Horion/Module/Modules/Criticals.h
@@ -3,11 +3,15 @@
 class Criticals :
 	public IModule
 {
+private:
+	float critChance = 100;
+	
 public:
 	Criticals();
 	~Criticals();
 
+	inline int getCritChance() { return critChance; };
+
 	// Inherited via IModule
 	virtual const char* getModuleName() override;
 };
-
