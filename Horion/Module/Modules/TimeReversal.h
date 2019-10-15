1464df1532248bf390beb2d444b6176aeb4c17e2
d1gress <55198830+d1gress@users.noreply.github.com>
2019-10-15 08:35:45 +0800

    make it have stuff

--- Horion/Module/Modules/TimeReversal.h
+++ Horion/Module/Modules/TimeReversal.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "Module.h"
+#include "../ModuleManager.h"
 
+class TimeReversal : public IModule
+{
+private:
+	int delay = 1;
+public:
+	TimeReversal();
+	~TimeReversal();
+
+	virtual const char* getModuleName() override;
+	virtual void onTick(C_GameMode* gm) override;
+	virtual void onEnable() override;
+};
